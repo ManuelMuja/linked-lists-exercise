ca3737476763bd14b1986495b0a4d63880ffb2a6
Manuel Iurissevich <manuel.iurissevich@hotmail.it>
2016-02-22 16:25:10 +0100

    Prepared headers for queue handling

--- src/queue.cpp
+++ src/queue.cpp
@@ -7,5 +7,165 @@
  *
  */
 
+#include "nodo.h"
 #include "queue.h"
+
+
+
+/*
+ * void Queue::init()
+ * init queue
+ *
+ */
+void Queue::init() {
+	
+} // init queue
+
+
+
+/*
+ * void Queue::push()
+ * append value to tail
+ *
+ */
+void Queue::push ( TIPO_DATO data ) {
+	
+} // append value to tail
+
+
+
+/*
+ * void Queue::push ( pQueue )
+ * append queue
+ *
+ */
+void Queue::push ( pQueue ) {
+	
+} // append queue
+
+
+
+/*
+ * void Queue::push ( TIPO_DATO data, int position )
+ * insert value into given position
+ *
+ */
+void Queue::push ( TIPO_DATO data, int position ) {
+	
+} // insert value into given position
+
+
+
+/*
+ * void Queue::push ( pQueue, int position )
+ * insert queue
+ *
+ */
+void Queue::push ( pQueue, int position ) {
+	
+} // insert queue
+
+
+
+/*
+ * TIPO_DATO Queue::pop()
+ * pop head
+ *
+ */
+TIPO_DATO Queue::pop() {
+	TIPO_DATO x;
+	
+	return x;	
+} // pop head
+
+
+
+/*
+ * void TIPO_DATO Queue::pop ( int position )
+ * pop data from given position
+ *
+ */
+TIPO_DATO Queue::pop ( int position ) {
+	TIPO_DATO x;
+	
+	return x;
+} // pop data from given position
+
+
+
+/*
+ * void Queue::setHead ( pNodo newHead )
+ * set a new head for the queue
+ *
+ */
+void Queue::setHead ( pNodo newHead ) {
+	
+} // set a new head for the queue
+ 
+
+
+/*
+ * void Queue::setTail ( pNodo newTail )
+ * set a new tail
+ *
+ */
+void Queue::setTail ( pNodo newTail ) {
+	
+} // set a new tail
+ 
+
+
+/*
+ * void Queue::shift()
+ * shift by one step
+ *
+ */
+void Queue::shift() {
+	
+} // shift by one step
+
+ 
+
+
+/*
+ * void Queue::shift ( int steps )
+ * shift by given steps
+ *
+ */
+void Queue::shift ( int steps ) {
+	
+} // shift by given steps
+
+
+
+/*
+ * void Queue::print()
+ * print whole queue
+ *
+ */
+void Queue::print() {
+	
+} // print whole queue
+ 
+
+
+/*
+ * void Queue::print ( int beg, int end )
+ * print from 'beg' to 'end'
+ *
+ */
+void Queue::print ( int beg, int end ) {
+	
+} // print from 'beg' to 'end'
+ 
+
+
+/*
+ * void Queue::empty()
+ * empty queue
+ *
+ */
+void Queue::empty() {
+	
+} // empty queue
 
--- src/queue.h
+++ src/queue.h
@@ -7,3 +7,29 @@
  *
  */
 
+class Queue {
+	
+private:
+	pNodo HEAD;
+	pNodo TAIL;
+	int lenght;
+	
+public:
+	void init();						// init queue
+	void push ( TIPO_DATO data );		// append value to tail
+	void push ( Queue* );				// append queue
+	void push ( TIPO_DATO data, int position );		// insert value into given position
+	void push ( Queue*, int position );				// insert queue
+	TIPO_DATO pop();					// pop head
+	TIPO_DATO pop ( int position );		// pop data from given position
+	void setHead ( pNodo newHead );		// set a new head for the queue
+	void setTail ( pNodo newTail );		// set a new tail
+	void shift();						// shift by one step
+	void shift ( int steps );			// shift by given steps
+	void print();						// print whole queue
+	void print ( int beg, int end );	// print from 'beg' to 'end'
+	void empty();						// empty queue
+}; // Queue
+
+typedef Queue* pQueue;
+
