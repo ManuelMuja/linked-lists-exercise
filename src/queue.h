169431b62da6bf2a66bce80875893bec5c097a65
Manuel Iurissevich <manuel.iurissevich@hotmail.it>
2016-02-23 09:06:33 +0100

    new set/get functions, new comments

--- src/queue.h
+++ src/queue.h
@@ -21,13 +21,17 @@ public:
 	void push ( TIPO_DATO data );		// append value to tail
 	void push ( Queue* );				// append queue
 	void push ( TIPO_DATO data, int position );		// insert value into given position
-	void push ( Queue*, int position );				// insert queue
+	void push ( Queue*, int position );				// TO DO: insert queue
 	TIPO_DATO pop();					// pop head
 	TIPO_DATO pop ( int position );		// pop data from given position
-	void setHead ( pNodo newHead );		// set a new head for the queue
-	void setTail ( pNodo newTail );		// set a new tail
-	void shift();						// shift by one step
-	void shift ( int steps );			// shift by given steps
+	void setHead ( pNodo newHead );		// set new head for this queue
+	void setTail ( pNodo newTail );		// set new tail
+	void setLenght ( int newLenght );	// set new lenght
+	pNodo getHead();					// get pointer to HEAD node of this queue
+	pNodo getTail();					// get pointer to TAIL node
+	int	getLenght();					// get lenght of this queue
+	void shift();						// TO DO: shift by one step
+	void shift ( int steps );			// TO DO:shift by given steps 
 	void print();						// print whole queue
 	void print ( int beg, int end );	// print from 'beg' to 'end'
 	void empty();						// empty queue
